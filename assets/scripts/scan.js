fa4c642a18070b762940f6dc0a3ed27dfaa6e4c5
Matt <matthew.jcarlton@outlook.com>
2021-05-09 15:06:43 +0100

    results only show when theyre given.

--- assets/scripts/scan.js
+++ assets/scripts/scan.js
@@ -40,7 +40,6 @@ var mainPage;
 })(mainPage || (mainPage = {}));
 
 $("#predict").click(async function(){
-	console.log("this is" + mainPage);
 	var prOutput = await mainPage.predictImage();
 	console.log(prOutput);
 	var class_names =['affenpinscher', 'afghan_hound', 'african_hunting_dog', 'airedale', 'american_staffordshire_terrier', 'appenzeller', 'australian_terrier', 'basenji', 'basset', 'beagle', 'bedlington_terrier', 'bernese_mountain_dog', 'black-and-tan_coonhound', 'blenheim_spaniel', 'bloodhound', 'bluetick', 'border_collie', 'border_terrier', 'borzoi', 'boston_bull', 'bouvier_des_flandres', 'boxer', 'brabancon_griffon', 'briard', 'brittany_spaniel', 'bull_mastiff', 'cairn', 'cardigan', 'chesapeake_bay_retriever', 'chihuahua', 'chow', 'clumber', 'cocker_spaniel', 'collie', 'curly-coated_retriever', 'dandie_dinmont', 'dhole', 'dingo', 'doberman', 'english_foxhound', 'english_setter', 'english_springer', 'entlebucher', 'eskimo_dog', 'flat-coated_retriever', 'french_bulldog', 'german_shepherd', 'german_short-haired_pointer', 'giant_schnauzer', 'golden_retriever', 'gordon_setter', 'great_dane', 'great_pyrenees', 'greater_swiss_mountain_dog', 'groenendael', 'ibizan_hound', 'irish_setter', 'irish_terrier', 'irish_water_spaniel', 'irish_wolfhound', 'italian_greyhound', 'japanese_spaniel', 'keeshond',
@@ -66,16 +65,23 @@ $("#predict").click(async function(){
 	// console.log(parsedData[0]); //top result
 	var prediction = class_names[parsedData[0].idx];
 	var confidence = parsedData[0].item;
+	document.getElementById("help-loading").style.display = "none";
+	document.getElementById("output").style.display = "block";
     $("#predictText").html("Our model predicts that your dog is a:");
 	$("#result").html(prediction);
 	confidence = (confidence*100).toFixed(2);
-	//$("#resultAcc").html("Percentage confidence = "+confidence+"%");
+	//$("#resultAcc").html("Percentage confidence = "+confidence+"%"); //% confidence
 	console.log(bestDog);
 });
 
+$("#predict").click(async function(){
+	document.getElementById("output").style.display = "none";
+	document.getElementById("result-container").style.display = "block";
+	document.getElementById("help-loading").style.display = "block";
+});
 
-$(document).ready(function(){});
-
+$(document).ready(function(){
+});
 //making the result container visible when pressing "predict breed"
 //make loading visible when pressing predict breed
 //when breed detected, make loading invisible
