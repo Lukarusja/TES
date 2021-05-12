4fee2b9a142fca32ca1cd3b0f47884253afa76b6
Matt <matthew.jcarlton@outlook.com>
2021-05-12 20:58:34 +0100

    new link

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
           <source src="assets/videos/test.webm">
           Your browser does not support the video tag.
         </video> -->
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/H4PSpR0p_sU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/watch?v=H4PSpR0p_sU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     <footer>
         <div class="footer-box">
